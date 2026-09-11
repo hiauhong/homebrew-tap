6c556e0e2b3d1c00ba4209b650eb4a74d548ed68
hiauhong <48238869+hiauhong@users.noreply.github.com>
2026-09-11 16:34:23 +0800

    remindkit v0.3.5

--- Formula/remindkit.rb
+++ Formula/remindkit.rb
@@ -1,12 +1,12 @@
 class Remindkit < Formula
   desc "Apple Reminders data pipeline CLI for AI agents"
   homepage "https://github.com/hiauhong/remindkit-cli"
-  version "0.3.4"
+  version "0.3.5"
   license "MIT"
 
   on_arm do
-    url "https://github.com/hiauhong/remindkit-cli/releases/download/v0.3.4/remindkit-darwin-arm64.tar.gz"
-    sha256 "3e616cd3b359c1bbfa008124e4cca295c8b435970a9f2bed5b788280296cbf8b"
+    url "https://github.com/hiauhong/remindkit-cli/releases/download/v0.3.5/remindkit-darwin-arm64.tar.gz"
+    sha256 "0640bf0c5b47dd3d1a8b1b0a0546c557a5333185e0c9307872f7c69d39a4c2b3"
   end
 
   on_intel do
